1ec9021feacf6c1c61e7a00ada8dd91843f925d6
Lucas Souza <lucasp.sdev@gmail.com>
2023-06-11 23:11:50 -0300

    feat: add click event to bring user to comment sections

--- src/views/Home/components/PlaylistSongItem/PlaylistSongItem.vue
+++ src/views/Home/components/PlaylistSongItem/PlaylistSongItem.vue
@@ -25,10 +25,20 @@ defineProps({
     </div>
 
     <div class="text-gray-600 text-lg">
-      <span class="comments">
-        <i class="fa fa-comments text-gray-600"></i>
-        {{ song.commentCount }}
-      </span>
+      <router-link
+        :to="{
+          name: 'Song',
+          params: { id: song.documentId },
+          hash: '#comments',
+        }"
+        v-slot="{ navigate }"
+        custom
+      >
+        <span class="comments" @click="navigate">
+          <i class="fa fa-comments text-gray-600"></i>
+          {{ song.commentCount }}
+        </span>
+      </router-link>
     </div>
   </li>
 </template>
